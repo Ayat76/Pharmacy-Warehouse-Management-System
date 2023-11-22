140413716996e7307ac848db93abd4c517d7dbff
loujain diab <loujaindiab551@gmail.com>
2023-11-22 14:09:18 +0300

    first brunch

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 
+use App\Http\Controllers\API\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
 });
 
 
