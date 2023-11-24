3c6180dcb298cf41b8c1b4c5180d6ea88d2a9ad9
loujain diab <loujaindiab551@gmail.com>
2023-11-24 13:13:14 +0300

    Revert "web.php edited online with Bitbucket"
    This reverts commit d5921981f26c04c5ff09b419b407b58575b61da0.

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
 
 
 
