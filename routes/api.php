bba8c7dc06586f3bd3479a761cd507c9812a26bd
Ayat AL_Helwani <ayat.hlw123@gamil.com>
2023-11-24 18:32:16 +0300

    finally

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,3 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::post('store',[\App\Http\Controllers\Web\WebMedicinesController::class,'store']);
 
 });
-// ayatttttttt
-// meow
-
