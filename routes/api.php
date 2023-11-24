2220c7d12e28aaa54a162d96dab453a552f045a9
loujain diab <loujaindiab551@gmail.com>
2023-11-24 15:39:30 +0300

    master modified

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
  return $request->user();
 });
 //register and login
+
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
